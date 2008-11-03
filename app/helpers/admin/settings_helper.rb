7401ef698428383df4c6d3b6aa7f42e0a649f84c
Mike Howe <mike@nutcracker.co.nz>
2008-11-03 15:37:31 +1300

    Provided default in case settings are nil

--- app/helpers/admin/settings_helper.rb
+++ app/helpers/admin/settings_helper.rb
@@ -1,5 +1,9 @@
 module Admin::SettingsHelper
   def settings_field(setting)
-    render :partial => "#{setting.field_type}", :locals => {:setting => setting}
+    if setting
+      render :partial => "#{setting.field_type}", :locals => {:setting => setting}
+    else
+      "No setting."
+    end
   end
 end
